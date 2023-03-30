2d3c5bc8e6156ee204d24e69c0d874b93390d4da
icat108 <ichacnakval@gmail.com>
2023-03-30 18:26:31 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import 'dotenv/config'
+import express from 'express'
+import nodeCleanup from 'node-cleanup'
+import routes from './routes.js'
+import { init, cleanup } from './whatsapp.js'
+import cors from 'cors'
+
+const app = express()
+
+const host = process.env.HOST || undefined
+const port = parseInt(process.env.PORT ?? 3000)
+
+app.use(cors())
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use(express.json({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use('/', routes)
+
+const listenerCallback = () => {
+    init()
+    console.log(`Server is listening on http://${host ? host : 'localhost'}:${port}`)
+}
+
+if (host) {
+    app.listen(port, host, listenerCallback)
+} else {
+    app.listen(port, listenerCallback)
+}
+
+nodeCleanup(cleanup)
+
+export default app
+
+
